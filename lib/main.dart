d30e2bd46222aac74e3b02d5376aa4de6af1a7ee
Anirban-Deb-Tanu <anirbandebtanu.uts@gmail.com>
2025-07-12 23:55:51 +0600

    Update main.dart
    Change Import Order

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:split_payment_app/firebase_options.dart';
 import 'package:split_payment_app/l10n/app_localizations.dart';
 import 'package:split_payment_app/src/shared/extensions/i18n_extension.dart';
 import 'package:split_payment_app/src/shared/providers/locale_provider.dart';
-import 'package:split_payment_app/firebase_options.dart';
 
 
 void main() async {
