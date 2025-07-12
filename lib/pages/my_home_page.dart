3eefffdebc1a8270835f9831af9131b8a31d425e
Anirban-Deb-Tanu <anirbandebtanu.uts@gmail.com>
2025-07-12 21:43:30 +0600

    Update my_home_page.dart

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../flavors.dart';
+import 'package:split_payment_app/flavors.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
