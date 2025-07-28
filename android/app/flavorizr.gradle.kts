f76f582e6d1fd9658cb2c35709f37f10a994a404
Anirban Deb <anirbandebtanu.uts@gmail.com>
2025-07-29 03:09:25 +0600

    feat: configure firebase

--- android/app/flavorizr.gradle.kts
+++ android/app/flavorizr.gradle.kts
@@ -8,12 +8,12 @@ android.apply {
     productFlavors {
         create("development") {
             dimension = "flavor-type"
-            applicationId = "com.ioaholdings.mobile.dev"
+            applicationId = "com.ioaholdings.mobile.splitpayment.dev"
             resValue(type = "string", name = "app_name", value = "Development App")
         }
         create("production") {
             dimension = "flavor-type"
-            applicationId = "com.ioaholdings.mobile.prd"
+            applicationId = "com.ioaholdings.mobile.splitpayment.prd"
             resValue(type = "string", name = "app_name", value = "Production App")
         }
     }
