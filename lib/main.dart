19278d3b572f697ad091d300250bf901a3cf3a90
Anirban Deb <anirbandebtanu.uts@gmail.com>
2025-07-30 12:49:24 +0600

    chore: update firebase app check configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'package:split_payment_app/app.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:split_payment_app/MyHomePage.dart';
 import 'package:split_payment_app/flavors.dart';
+import 'package:split_payment_app/l10n/app_localizations.dart';
+import 'package:split_payment_app/src/shared/providers/locale_provider.dart';
 
-void main() {
+void main() async {
   F.appFlavor = Flavor.values.firstWhere(
     (element) => element.name == appFlavor,
   );
 
-  runApp(const App());
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locale = ref.watch(appLocaleProvider);
+
+    return MaterialApp(
+      title: 'Split Payment App new',
+      locale: locale,
+      //locale: const Locale('ja'),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ja'),
+      ],
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
 }
